edef072767a142fc30cd6e12714f38e615e6a9fe
P1nH4ck <130668406+P1nH4ck@users.noreply.github.com>
2023-04-21 12:16:48 +0800

    Update hello.go

--- hello.go
+++ hello.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
     urlValues := url.Values{}
-    urlValues.Add("name","zhaofan33")
+    urlValues.Add("name","zhaofan44")
     urlValues.Add("age","223")
     resp, _ := http.PostForm("http://www.baidu.com",urlValues)
     body, _ := ioutil.ReadAll(resp.Body)
